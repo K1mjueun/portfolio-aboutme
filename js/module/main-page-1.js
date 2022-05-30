3f4a4989cd661d0a60b135e03f3e283d1ca17cdd
K1mjueun <jueun4779@gmail.com>
2022-05-30 10:50:48 +0900

    main page 1 마무리

--- js/module/main-page-1.js
+++ js/module/main-page-1.js
@@ -14,6 +14,14 @@ const toypjMemo = document.querySelector(".toyproject-memo");
 
 const bling = document.querySelector('.my-photo-mini > section:nth-child(2)');
 
+const mpclick1 = document.querySelector('#main-page-1 > #note-board >.click-1');
+const mpclick2 = document.querySelector('#main-page-1 > #note-board >.click-2');
+const mpclick3 = document.querySelector('#main-page-1 > #note-board >.click-3');
+
+const mparrow1 = document.querySelector('#main-page-1 > #note-board >.arrow-1');
+const mparrow2 = document.querySelector('#main-page-1 > #note-board >.arrow-2');
+const mparrow3 = document.querySelector('#main-page-1 > #note-board >.arrow-3');
+
 let windieMemoWidth = 350;
 let windieMemoHeight = 400;
 
@@ -23,12 +31,13 @@ let bubbleroomMemoHeight = 410;
 let toypjMemoWidth = 300;
 let toypjMemoHeight = 450;
 
-mouseoverBigger(windieMemo, windieMemoWidth, windieMemoHeight);
-mouseoverBigger(bubbleroomMemo, bubbleroomMemoWidth, bubbleroomMemoHeight);mouseoverBigger(toypjMemo,toypjMemoWidth, toypjMemoHeight);
+mouseoverBigger(windieMemo, mpclick1, mparrow1,windieMemoWidth, windieMemoHeight);
+mouseoverBigger(bubbleroomMemo, mpclick2, mparrow2, bubbleroomMemoWidth, bubbleroomMemoHeight);
+mouseoverBigger(toypjMemo, mpclick3, mparrow3,toypjMemoWidth, toypjMemoHeight);
 
-mouseleaveSmaller(windieMemo, windieMemoWidth, windieMemoHeight);
-mouseleaveSmaller(bubbleroomMemo, bubbleroomMemoWidth, bubbleroomMemoHeight);mouseleaveSmaller(toypjMemo,toypjMemoWidth, toypjMemoHeight);
-
+mouseleaveSmaller(windieMemo, mpclick1, mparrow1,windieMemoWidth, windieMemoHeight);
+mouseleaveSmaller(bubbleroomMemo, mpclick2, mparrow2,bubbleroomMemoWidth, bubbleroomMemoHeight);
+mouseleaveSmaller(toypjMemo, mpclick3, mparrow3,toypjMemoWidth, toypjMemoHeight);
 
 let blingBingle = [
   {transform : 'rotate(0deg)'},
