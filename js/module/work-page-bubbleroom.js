5b89d4f94dae5e6191744b98b40c4f29413fbe3d
K1mjueun <jueun4779@gmail.com>
2022-06-02 11:59:52 +0900

    버블룸, 토이프로젝트 작업화면

---
+++ js/module/work-page-bubbleroom.js
@@ -0,0 +1,4 @@
+// work-page-windie
+const wpBubbleroom = document.querySelector("#work-page-bubbleroom");
+
+displayChange(bubbleroomMemo,mainPage1,wpBubbleroom);
---
+++ js/module/work-page-toyproject.js
@@ -0,0 +1,4 @@
+// work-page-windie
+const wpToyproject = document.querySelector("#work-page-toyproject");
+
+displayChange(toypjMemo,mainPage1,wpToyproject);
