ef6c38e83620e5eedebce903242c0f06a8126b0e
K1mjueun <jueun4779@gmail.com>
2022-06-02 17:10:38 +0900

    쿼리셀렉터로 화살표 스티커를 한번에 제어하려고 하다가 꼬여서 다 삭제했따, 따로따로 설정하기

--- js/module/windie-process-page.js
+++ js/module/windie-process-page.js
@@ -14,3 +14,11 @@ displayChange(ProbackBtn[0],windieProPage,wpWindie);
 
 const topBtn = document.querySelectorAll(".top-sticker");
 tothetop(topBtn[0]);
+
+console.log(ProbackBtn);
+console.log(ProbackBtn[0]);
+console.log(ProbackBtn[1]);
+console.log(ProbackBtn[2]);
+console.log(topBtn);
+
+// 쿼리셀렉터로 한번에 잡아서 제어하려고 했더니 아주 꼬여서 다시해야된다. 그냥 따로따로 하자!!
