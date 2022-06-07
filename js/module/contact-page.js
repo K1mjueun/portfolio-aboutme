15311cce607e44a9fdc2abd7979ec9c0718c2ca4
K1mjueun <jueun4779@gmail.com>
2022-06-07 11:37:17 +0900

    contact stamp를 누르면 마지막 페이지, 연락처 페이지로 넘어간다

--- js/module/contact-page.js
+++ js/module/contact-page.js
@@ -1,1 +1,12 @@
-// contact-page
+// contact page
+const contactPage = document.querySelector("#contact-page");
+contactPage.style.display ="none";
+console.log(contactPage);
+
+console.log(contactStamp1);
+console.log(contactStamp2);
+console.log(contactStamp3);
+
+displayChange(contactStamp1,wpWindie,contactPage);
+displayChange(contactStamp2,wpBubbleroom,contactPage);
+displayChange(contactStamp3,wpToyproject,contactPage);
